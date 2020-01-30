1d15cee244563779aeb8d26c6239619293cb0fad
Andrew Frazier <acorbettfrazier@gmail.com>
2020-01-29 21:02:40 -0500

    building counter

--- war.js
+++ war.js
@@ -1,8 +1,13 @@
 // DECK BUILD
 // ACE HIGH
 
-cards = ["AofS", "1ofS" "2ofS" "3ofS" "4ofS" "5ofS" "6ofS" "7ofS" "8ofS" "9ofS" "JofS" "QofS" "KofS"];
-
+const cards = {
+  s: ["2, 3, 4, 5, 6, 7, 8, 9, J, Q, K, A"],
+  h: ["2, 3, 4, 5, 6, 7, 8, 9, J, Q, K, A"],
+  d: ["2, 3, 4, 5, 6, 7, 8, 9, J, Q, K, A"],
+  c: ["2, 3, 4, 5, 6, 7, 8, 9, J, Q, K, A"]
+};
+console.log(cards.hearts);
 // SPLIT THE DECK
 
 // PLACING OF EACH PLAYERS CARD
@@ -12,7 +17,7 @@ cards = ["AofS", "1ofS" "2ofS" "3ofS" "4ofS" "5ofS" "6ofS" "7ofS" "8ofS" "9ofS" 
 // CARDS BEING PLACED BACK TO STACK OF WINNING PLAYER
 
 // WAR GAME _____------_____-----______----______-----
-
+if (s[] == )
 // PLACEMENT OF 3 CARDS FROM EACH PLAYERS STACK FACE DOWN
 
 // PLACE OF 1 CARD UP FROM EACH STACK
@@ -28,8 +33,15 @@ cards = ["AofS", "1ofS" "2ofS" "3ofS" "4ofS" "5ofS" "6ofS" "7ofS" "8ofS" "9ofS" 
 // PLAYER'S CARDS REACH 0
 
 // COUNTING OF ROUNDS
-var rounds = function round() {};
+// var rounds = function round() {};
 
+var totalRounds = {
+    total: 0
+};
+var round = function count() {
+    if (player1.cards -= 1 && player2.cards -= 1)
+        
+}
 // COUNTING OF WAR ROUNDS
 
 // COUNTING OF BOTH ROUNDS TOTAL
