62d7d389a36dc24c24e1fb7a236114aec52980e3
Andrew Frazier <acorbettfrazier@gmail.com>
2020-01-29 22:07:49 -0500

    added functional ideas

--- war.js
+++ war.js
@@ -1,66 +1,116 @@
 // DECK BUILD
 // ACE HIGH
 
-const cards = {
-  s: ["2, 3, 4, 5, 6, 7, 8, 9, J, Q, K, A"],
-  h: ["2, 3, 4, 5, 6, 7, 8, 9, J, Q, K, A"],
-  d: ["2, 3, 4, 5, 6, 7, 8, 9, J, Q, K, A"],
-  c: ["2, 3, 4, 5, 6, 7, 8, 9, J, Q, K, A"]
-};
-console.log(cards.hearts);
-
 // PLAYER 1
 const playerOne = {
-
+    cards: [],
 }
 
 // PLAYER 2
 const playerTwo = {
+    cards: [],
 
 }
 
 // SPLIT THE DECK
 
+
 // PLACING OF EACH PLAYERS CARD
 
-// HIGHER CARD WINS
+// learn about annonomous function. meaning one that calls itself 
+
+
+
+// WAR FUNCTION
+function war(a,b) {
+    if (playerOne(a) === playerTwo(b)) {
+    return alert("WAR!!!!!")
+}   else {
+      play();  
+}}
+// 
 
 // CARDS BEING PLACED BACK TO STACK OF WINNING PLAYER
+
 
 // WAR GAME _____------_____-----______----______-----
 if (s[] == )
 // PLACEMENT OF 3 CARDS FROM EACH PLAYERS STACK FACE DOWN
 
+
 // PLACE OF 1 CARD UP FROM EACH STACK
+
 
 // WINNER
 
+
 // TIE OR REPEAT
+
 
 // COLLECTION OF CARDS AFTER WIN
 
+
 // END OF WAR GAME____-----______----____-----
 
+
 // PLAYER'S CARDS REACH 0
+
 
 
 
 // COUNTING OF ROUNDS
 // var rounds = function round() {};
 
-var totalRounds = {
-    total: 0
-};
+
 var round = function count() {
     if (player1.cards -= 1 && player2.cards -= 1)
         return totalRounds.total;
 }
 // COUNTING OF WAR ROUNDS
 
+
 // COUNTING OF BOTH ROUNDS TOTAL
 
+
+// ROCK PAPER SCISCORS TIE
+
+// Print a message for each "round" of the game showing the 
+// cards played by each user, who 
+// won the round, and how many cards each player now has.
+
+// function counter(){
+// if (totalRounds += 1){
+//     return alert("Round" + totalRounds)
+// }   else if (playerOne.cards === 0){
+//         return alert("Player 1 Wins!")
+// }   else if (playerTwo.cards === 0) {
+//         return alert("Player 2 Wins!")
+// }   else play()
+
+// };
+
+
+
+
+
+
+
+
+
+// COUNTING ROUNDS
+var totalRounds = {
+    total: 0
+};
+
+function counter(){
+    if (totalRounds.total += 1){
+        return alert("Round" + totalRounds.total)
+    }};
+// 
+
+
 // ALERT (PLAYER) WINS 
- function endGame() {
+function endGame() {
     if (playerOne.cards === 0) {
         return alert("Player 1 Wins!")}   
     else if (playerTwo.cards === 0) {
@@ -71,6 +121,13 @@ var round = function count() {
     // at end of play function
  }
 }
+// 
 
-
-
+// THE DECK
+const cards = {
+    s: ["2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14"],
+    h: ["2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14"],
+    d: ["2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14"],
+    c: ["2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14"]
+  };
+//   
