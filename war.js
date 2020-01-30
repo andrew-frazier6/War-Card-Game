f681cfc89493557d6b3198a60007185240dae081
Andrew Frazier <acorbettfrazier@gmail.com>
2020-01-29 20:12:03 -0500

    made read me

---
+++ war.js
@@ -0,0 +1,36 @@
+// DECK BUILD 
+
+
+
+// SPLIT THE DECK 
+
+
+
+// PLACING OF EACH PLAYERS CARD 
+
+
+- Higher card takes both cards
+
+- Cards place to bottom of winner's stack
+
+- tie **WAR**
+
+- each player places 3 face down
+
+- Each place one face up
+
+- Higher card takes whole pot
+
+- If tie, repeat until no tie
+
+- Person loses all cards
+
+  - maybe a listener or function calling for when players cards array reaches 0
+
+**Things to remember**
+
+- count each round of hands, including war rounds
+
+- ace is high, so set correct placement of index
+
+- if one person is not able to finish a round of war because they dont have enough cards then they lose
