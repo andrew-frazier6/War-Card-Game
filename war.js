39667fd96a2e0d481fac0ae9078510b4b3377d26
Andrew Frazier <acorbettfrazier@gmail.com>
2020-02-02 15:12:20 -0500

    square 1. build deck

--- war.js
+++ war.js
@@ -1,71 +1,106 @@
-// PLAYER
-class Player {
-  constructor(name) {
-    this.name = name;
-    this.hand = [];
-    this.winnings = [];
-  }
-}
-// PLAYER INSTANCE
-const player1 = new Player("player 1");
-const player2 = new Player("player 2");
-// PLAYING TABLE
-class Table {
-  constructor(players) {
-    this.players = players;
-    this.inPlay = [];
-  }
-}
-// CARD
-class Card {
-  constructor(suit, face) {
-    this.suit = suit;
-    this.face = face;
-  }
-}
-//  DECK
-class Deck {
-  constructor() {
-    this.deck = [];
-  }
-  // BUILD DECK
-  buildDeck(suits, faces) {
-    for (let suit of suits) {
-      for (let face of faces) {
-        this.deck.push(new Card(suit, face));
-      }
-    }
-    return this.deck;
-  }
-  // SHUFFLE DECK
-  shuffle() {
-    let counter = this.deck.length,
-      temp,
-      i;
+let newBoxDeck = [];
+let suit = ["Hearts", "Spades", "Diamonds", "Clubs"];
+let rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+let faces = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+let playerOne = [];
+let playerTwo = [];
+let playerOneTable = [];
+let playerTwoTable = [];
 
-    while (counter) {
-      i = Math.floor(Math.random() * counter--);
-      temp = this.deck[counter];
-      this.deck[counter] = this.deck[i];
-      this.deck[i] = temp;
+// PLAYER
+// class Player {
+//   constructor(name) {
+//     this.name = name;
+//     this.hand = [];
+//     this.winnings = [];
+//   }
+// }
+// PLAYER INSTANCE
+// const player1 = new Player("player 1");
+// const player2 = new Player("player 2");
+// PLAYING TABLE
+// class Table {
+//   constructor(players) {
+//     this.players = players;
+//     this.inPlay = [];
+//   }
+// }
+// CARD
+// class Card {
+//   constructor(suit, rank, face) {
+//     this.suit = suit;
+//     this.rank = rank;
+//     this.face = face;
+//   }
+// }
+//  DECK #5
+function buildDeck() {
+  for (let i = 0; i < suit.length; i++) {
+    for (let j = 0; j < rank.length; j++) {
+      let playingCard = { rank: rank[i], suit: suit[j], face: face[j] };
+      newBoxDeck.push(playingCard);
     }
-    return this.deck;
-  }
-  // DEAL DECK
-  deal() {
-    while (Player.hand.length < 26) {
-      Player.hand.push(this.deck.pop());
-    }
-    console.log(Player.hand);
   }
 }
+buildDeck();
+console.log(newBoxDeck);
+
+// BUILD DECK #3
+//   buildDeck() {
+//     let suit = ["Hearts", "Spades", "Diamonds", "Clubs"];
+//     let face = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+//     let rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+//     for (let i = 0; i < suit.length; i++) {
+//       for (let j = 0; j < rank.length; j++) {
+//         this.deck.push(new Card(suit[i], face[j], rank[j]));
+//       }
+//     }
+//   }
+// }
+// BUILD DECK #2
+// buildDeck(suits, faces) {
+//   for (let suit of suits) {
+//     for (let face of faces) {
+//       this.deck.push(new Card(suit, face));
+//     }
+//   }
+//   return this.deck;
+// }
+// SHUFFLE DECK
+// shuffle() {
+//   let counter = this.deck.length,
+//     temp,
+//     i;
+
+//   while (counter) {
+//     i = Math.floor(Math.random() * counter--);
+//     temp = this.deck[counter];
+//     this.deck[counter] = this.deck[i];
+//     this.deck[i] = temp;
+//   }
+// }
+// DEAL DECK
+//   deal() {
+//     while (Player.hand.length < 26) {
+//       Player.hand.push(this.deck.pop());
+//     }
+//   }
+// }
+
+// let boxDeck = new Deck();
+// console.log(buildDeck());
+
 // DECK CONTENTS
-const suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
-const faces = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-let deck = new Deck();
-deck.buildDeck(suits, faces);
-deck.shuffle();
-deck.deal();
+// const suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+// const faces = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+// let deck = new Deck();
+// deck.buildDeck(suits, faces);
+// deck.shuffle();
+// deck.deal();
+
+// let play = () => {
+//   Table.push(player1[0], player2[0]);
+// };
 // TOTAL ROUNDS
 // let totalRounds = [];
 // function roundCounter(){
