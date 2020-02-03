f6b2aa58bacfd9ec44041a6e77656af52293dc5a
Andrew Frazier <acorbettfrazier@gmail.com>
2020-02-03 08:51:30 -0500

    game is running

--- war.js
+++ war.js
@@ -1,31 +1,4 @@
-// PLAYER
-// class Player {
-//   constructor(name) {
-//     this.name = name;
-//     this.hand = [];
-//     this.winnings = [];
-//   }
-// }
-// PLAYER INSTANCE
-// const player1 = new Player("player 1");
-// const player2 = new Player("player 2");
-// PLAYING TABLE
-// class Table {
-//   constructor(players) {
-//     this.players = players;
-//     this.inPlay = [];
-//   }
-// }
-// CARD
-// class Card {
-//   constructor(suit, rank, face) {
-//     this.suit = suit;
-//     this.rank = rank;
-//     this.face = face;
-//   }
-// }
-//
-//  DECK #5 BUILT
+//  VARIABLES
 let newBoxDeck = [];
 let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
 let rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
@@ -34,7 +7,7 @@ let playerTwo = [];
 let playerOneTable = [];
 let playerTwoTable = [];
 let table = [];
-
+//  BUILD DECK
 function buildDeck() {
   let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
   let rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
@@ -46,8 +19,9 @@ function buildDeck() {
   }
   console.log(newBoxDeck);
 }
-buildDeck();
-// SHUFFLE
+
+// SHUFFLE                    FISHER YATES ALGO
+// https://www.tutorialspoint.com/what-is-fisher-yates-shuffle-in-javascript
 let shuffleDeck = newBoxDeck;
 function shuffle() {
   for (let i = shuffleDeck.length - 1; i > 0; i--) {
@@ -56,10 +30,9 @@ function shuffle() {
     shuffleDeck[i] = shuffleDeck[j];
     shuffleDeck[j] = temp;
   }
-  return shuffleDeck;
+  console.log(shuffleDeck);
 }
-shuffle();
-
+// DEALING THE CARDS
 let dealingCards = shuffleDeck;
 function deal() {
   for (let i = 0; i < newBoxDeck.length; i++) {
@@ -69,11 +42,10 @@ function deal() {
       playerTwo.push(newBoxDeck[i]);
     }
   }
+  console.log(playerOne, playerTwo);
 }
-deal();
-console.log(playerOne, playerTwo);
 
-// A ROUND
+// PLAYING A ROUND AND SHOWING THE CARDS ON THE TABLE 1 BY 1
 function showCard() {
   if (playerOne.length && playerTwo.length !== 0) {
     playerOneTable.push(playerOne[0]), playerTwoTable.push(playerTwo[0]);
@@ -81,33 +53,53 @@ function showCard() {
     playerTwo.splice(0, 1);
   }
 }
-
+// CHECKING TO SEE THE HIGH CARD
 function highCard() {
-  if (playerOneTable[0].rank > playerTwoTable[1].rank) {
-    console.log("Winner. Player 1");
-    playerOne.push(playerOneTable[0], playerTwoTable[1]);
-  } else if (playerTwoTable[1].rank > playerOneTable[0].rank) {
-    console.log("Winner. Player 2");
-    playerTwo.push(playerTwoTable[0], playerOneTable[1]);
-  } else playerOneTable[0].rank === playerTwoTable[1].rank;
-  console.log("war");
-}
-
-console.log(playerOneTable, playerTwoTable);
-showCard();
-console.log(playerOneTable, playerTwoTable);
-highCard();
-
-// END GAME
-function endGame() {
-  if ((playerOne.length = 52)) {
-    console.log("Player 1 won the war!");
-  } else if ((playerTwo.length = 52)) {
-    console.log("Player 2 won the war!");
+  if (playerOneTable[0].rank > playerTwoTable[0].rank) {
+    console.log("Round Winner. Player 1");
+    playerOne.push(playerOneTable[0], playerTwoTable[0]);
+  } else if (playerTwoTable[0].rank > playerOneTable[0].rank) {
+    console.log("Round Winner. Player 2");
+    playerTwo.push(playerTwoTable[0], playerOneTable[0]);
   } else {
-    play();
+    war();
   }
 }
+
+function War() {
+  playerOneTable.push(playerOne.splice(0, 4)),
+    playerTwoTable.push(playerTwo.slice(0, 4));
+  compareWar();
+}
+
+function compareWar() {
+  if (playerOneTable[0].rank > playerTwoTable[0].rank) {
+    console.log("Winner of WAR. Player 1");
+    playerOne.push(playerOneTable[0], playerTwoTable[0]);
+  } else if (playerTwoTable[0].rank > playerOneTable[0].rank) {
+    console.log("Winner of WAR. Player 2");
+    playerTwo.push(playerTwoTable[0], playerOneTable[0]);
+
+    playerOne.push(...playerOneTable), playerTwo.push(playerTwoTable);
+  }
+}
+// WAR FUNCTION AND CONDITIONS
+
+// END GAME CHECK
+function endGameCheck() {
+  while (playerOne.length || playerTwo.length === 52) {
+    playGame();
+  }
+}
+//  FULL PLAY GAME FUNCTION
+function playGame() {
+  buildDeck();
+  shuffle();
+  deal();
+  showCard();
+  highCard();
+}
+playGame();
 // BUILD DECK #3
 //   buildDeck() {
 //     let suit = ["Hearts", "Spades", "Diamonds", "Clubs"];
@@ -232,17 +224,30 @@ function endGame() {
 //  }
 // }
 
-// addCardsInPlay()
-// conductTurn()
-// finishTurn()
-// conductTurn()
-// warDeclared = boolean;
-// prepareForWar()
-// receiveSpoilsOfWar()
-// spoilsOfWar = [];
-// winnerOfRound = null;
-// assignWinnerCards()
-// determineWinner()
-
-// startGame()
-// endGame()
+// PLAYER
+// class Player {
+//   constructor(name) {
+//     this.name = name;
+//     this.hand = [];
+//     this.winnings = [];
+//   }
+// }
+// PLAYER INSTANCE
+// const player1 = new Player("player 1");
+// const player2 = new Player("player 2");
+// PLAYING TABLE
+// class Table {
+//   constructor(players) {
+//     this.players = players;
+//     this.inPlay = [];
+//   }
+// }
+// CARD
+// class Card {
+//   constructor(suit, rank, face) {
+//     this.suit = suit;
+//     this.rank = rank;
+//     this.face = face;
+//   }
+// }
+//
